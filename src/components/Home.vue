0b54a54c7b33f8d4b13626d8c777fbfc6fa1225e
fuwangming <512048841@qq.com>
2021-08-31 21:34:33 +0800

    登录退出功能开发完成

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <el-button @click="logoutbtn" type='info'>退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logoutbtn(){
+      window.sessionStorage.clear();
+      this.$router.push('/login');
+      this.$message.success('退出成功！')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
