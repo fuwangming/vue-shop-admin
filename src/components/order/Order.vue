c0b43aecfb0aae97cb9e6095a6eaea12f4be4126
fuwangming <512048841@qq.com>
2021-09-08 23:29:33 +0800

    修复物流接口问题

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -38,14 +38,13 @@
               size="mini"
               icon="el-icon-edit"
               @click="showEditDialog"
-            ></el-button>
+            >地址</el-button>
             <el-button
-              disabled
               type="success"
               size="mini"
               icon="el-icon-location"
               @click="showProgressDialog"
-            ></el-button>
+            >物流</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -169,12 +168,78 @@ export default {
     },
     // 物流对话框
     async showProgressDialog() {
-      // 供测试的物流单号：1106975712662
-      const { data: res } = await this.$http.get("/kuaidi/4316859604291");
-      if (res.meta.status !== 200) {
-        return this.$message.error("获取物流进度失败!");
-      }
-      this.progressInfo = res.data;
+      // // 供测试的物流单号：1106975712662
+      // const { data: res } = await this.$http.get("/kuaidi/4316859604291");
+      // if (res.meta.status !== 200) {
+      //   return this.$message.error("获取物流进度失败!");
+      // }
+      // this.progressInfo = res.data;
+
+      // 接口有问题，所以直接定义数据
+      this.progressInfo = [
+        {
+          time: "2018-05-10 09:39:00",
+          ftime: "2018-05-10 09:39:00",
+          context: "已签收,感谢使用顺丰,期待再次为您服务",
+          location: "",
+        },
+        {
+          time: "2018-05-10 08:23:00",
+          ftime: "2018-05-10 08:23:00",
+          context:
+            "[北京市]北京海淀育新小区营业点派件员 顺丰速运 95338正在为您派件",
+          location: "",
+        },
+        {
+          time: "2018-05-10 07:32:00",
+          ftime: "2018-05-10 07:32:00",
+          context: "快件到达 [北京海淀育新小区营业点]",
+          location: "",
+        },
+        {
+          time: "2018-05-10 02:03:00",
+          ftime: "2018-05-10 02:03:00",
+          context:
+            "快件在[北京顺义集散中心]已装车,准备发往 [北京海淀育新小区营业点]",
+          location: "",
+        },
+        {
+          time: "2018-05-09 23:05:00",
+          ftime: "2018-05-09 23:05:00",
+          context: "快件到达 [北京顺义集散中心]",
+          location: "",
+        },
+        {
+          time: "2018-05-09 21:21:00",
+          ftime: "2018-05-09 21:21:00",
+          context: "快件在[北京宝胜营业点]已装车,准备发往 [北京顺义集散中心]",
+          location: "",
+        },
+        {
+          time: "2018-05-09 13:07:00",
+          ftime: "2018-05-09 13:07:00",
+          context: "顺丰速运 已收取快件",
+          location: "",
+        },
+        {
+          time: "2018-05-09 12:25:03",
+          ftime: "2018-05-09 12:25:03",
+          context: "卖家发货",
+          location: "",
+        },
+        {
+          time: "2018-05-09 12:22:24",
+          ftime: "2018-05-09 12:22:24",
+          context: "您的订单将由HLA（北京海淀区清河中街店）门店安排发货。",
+          location: "",
+        },
+        {
+          time: "2018-05-08 21:36:04",
+          ftime: "2018-05-08 21:36:04",
+          context: "商品已经下单",
+          location: "",
+        },
+      ];
 
       this.progressDialogVisible = true;
     },
